536f0542550cc06950aa46bb5ad3180fb86e5bc0
aleksei@mazur <mazuralexey@gmail.com>
2022-09-15 06:27:51 +0300

    formatting styles, rewrite js to OOP style

--- js/main.js
+++ js/main.js
@@ -1,28 +1,47 @@
-const products = [
-    { title: 'Shirt', price: 1500},
-    { title: 'Socks', price: 5000},
-    { title: 'Jacket', price: 3500},
-    { title: 'Shoes', price: 25000},
-    { title: 'Shirt', price: 1500 },
-    { title: 'Socks', price: 5000 },
-    { title: 'Jacket', price: 3500 },
-    { title: 'Shoes', price: 2500 },
-    
-];
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render(); //вывод товаров на страницу
+    }
 
-//Функция для формирования верстки каждого товара
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Jacket', price: 3500 },
+            { id: 2, title: 'Shoes', price: 25000 },
+            { id: 3, title: 'Shirt', price: 1500 },
+            { id: 4, title: 'Socks', price: 5000 },
+            { id: 5, title: 'Jacket', price: 3500 },
+            { id: 6, title: 'Shoes', price: 2500 },
+        ];
+    }
 
-const renderProduct = (product, img ='https://dummyimage.com/320x240/331e33/f7f7f7.png') => {
-    return `<div class="product-item">
-                    <img src="${img}"/>
-                    <h3>${product.title}</h3>
-                    <p>${product.price}</p>
-                    <button class="buy-btn">Купить</button>
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://dummyimage.com/320x240/331e33/f7f7f7.png') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
             </div>`
-};
+    }
+}
 
-const renderPage = list => {
-    document.querySelector('.products').innerHTML = 
-        (list.map(product => renderProduct(product))).join('')
-};
-renderPage(products);
+let list = new ProductList();
+
